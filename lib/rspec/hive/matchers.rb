fb2a34604a6e79f28c5f443a16011d702d8ea4bb
Michał Knapik <michal.knapik@u2i.com>
2016-05-09 19:55:11 +0200

    Match result set with no defined order

--- lib/rspec/hive/matchers.rb
+++ lib/rspec/hive/matchers.rb
@@ -3,20 +3,27 @@ require 'rspec/matchers'
 RSpec::Matchers.define :match_result_set do |expected|
   match do |actual|
     @diffable_actual = []
+
+    @actual = actual.clone
+
     expected.map.with_index do |expected_row, i|
       if expected_row.respond_to?(:each_pair)
         if @partial_match
-          @diffable_actual << actual[i].select { |k, _v| expected_row.keys.include?(k) }
-          selected_actual_values = actual[i].values_at(*expected_row.keys)
-          values_match?(expected_row.values, selected_actual_values)
+          result_set_match?(
+            actual[i], expected_row,
+            expected_transformer: ->(e) { e.values },
+            actual_transformer: ->(candidate) { candidate.values_at(*expected_row.keys) },
+            diffable_transformer: ->(candidate) { candidate.select { |k, _v| expected_row.keys.include?(k) } }
+          )
         else
-          @diffable_actual << actual[i]
-          values_match?(expected_row, actual[i])
+          result_set_match?(actual[i], expected_row)
         end
       elsif expected_row.respond_to?(:each)
         raise ArgumentError, "Can't use partially matcher with Arrays" if @partial_match
-        @diffable_actual << actual[i].values
-        values_match?(expected_row, actual[i].values)
+        result_set_match?(
+          actual[i], expected_row,
+          actual_transformer: ->(candidate) { candidate.values }
+        )
       else
         raise ArgumentError, 'Unknown type'
       end
@@ -25,6 +32,39 @@ RSpec::Matchers.define :match_result_set do |expected|
 
   chain :partially do
     @partial_match = true
+  end
+
+  chain :unordered do
+    @unordered = true
+  end
+
+  def result_set_match?(
+    actual, expected_row,
+    expected_transformer: ->(expected) { expected },
+    actual_transformer: ->(candidate) { candidate },
+    diffable_transformer: actual_transformer
+  )
+    if @unordered
+      unordered_result_set_match?(expected_row, expected_transformer, actual_transformer, diffable_transformer)
+    else
+      ordered_result_set_match?(actual, expected_row, expected_transformer, actual_transformer, diffable_transformer)
+    end
+  end
+
+  def unordered_result_set_match?(expected_row, expected_transformer, actual_transformer, diffable_transformer)
+    found_index = @actual.find_index do |candidate|
+      values_match?(expected_transformer.call(expected_row), actual_transformer.call(candidate))
+    end
+    return false unless found_index
+    found = @actual[found_index]
+    @actual.delete_at(found_index)
+    @diffable_actual << diffable_transformer.call(found)
+    true
+  end
+
+  def ordered_result_set_match?(actual, expected_row, expected_transformer, actual_transformer, diffable_transformer)
+    @diffable_actual << diffable_transformer.call(actual)
+    values_match?(expected_transformer.call(expected_row), actual_transformer.call(actual))
   end
 
   failure_message do |actual|
--- spec/lib/rspec/hive/matchers_spec.rb
+++ spec/lib/rspec/hive/matchers_spec.rb
@@ -8,12 +8,23 @@ RSpec.describe 'match_result_set' do
   let(:paul) { {first_name: 'Paul', last_name: 'McCartney', age: 73} }
 
   let(:full_match) { expect(actual_rows).to match_result_set(expected_rows) }
+  let(:unordered_match) { expect(actual_rows).to match_result_set(expected_rows).unordered }
   let(:partial_match) { expect(actual_rows).to match_result_set(expected_rows).partially }
+  let(:partial_unordered_match) { expect(actual_rows).to match_result_set(expected_rows).partially.unordered }
+
   let(:full_match_fails) { expect(actual_rows).not_to match_result_set(expected_rows) }
+  let(:unordered_match_fails) { expect(actual_rows).not_to match_result_set(expected_rows).unordered }
   let(:partial_match_fails) { expect(actual_rows).not_to match_result_set(expected_rows).partially }
+  let(:partial_unordered_match_fails) { expect(actual_rows).not_to match_result_set(expected_rows).partially.unordered }
 
   let(:partial_match_raises_error) do
-    expect { partial_match }.to raise_error(ArgumentError, "Can't use partially matcher with Arrays")
+    error_message = "Can't use partially matcher with Arrays"
+    aggregate_failures do
+      expect { partial_match }.to raise_error(ArgumentError, error_message)
+      expect { partial_match_fails }.to raise_error(ArgumentError, error_message)
+      expect { partial_unordered_match }.to raise_error(ArgumentError, error_message)
+      expect { partial_unordered_match_fails }.to raise_error(ArgumentError, error_message)
+    end
   end
 
   context 'when the expected set has only one row' do
@@ -24,6 +35,7 @@ RSpec.describe 'match_result_set' do
         let(:expected_rows) { [john.values] }
 
         specify { full_match_fails }
+        specify { unordered_match_fails }
         specify { partial_match_raises_error }
       end
 
@@ -31,7 +43,9 @@ RSpec.describe 'match_result_set' do
         let(:expected_rows) { [john] }
 
         specify { full_match_fails }
+        specify { unordered_match_fails }
         specify { partial_match_fails }
+        specify { partial_unordered_match_fails }
       end
     end
 
@@ -43,6 +57,7 @@ RSpec.describe 'match_result_set' do
           let(:expected_rows) { [john.values << 'yoko'] }
 
           specify { full_match_fails }
+          specify { unordered_match_fails }
           specify { partial_match_raises_error }
         end
 
@@ -50,6 +65,7 @@ RSpec.describe 'match_result_set' do
           let(:expected_rows) { [paul.values] }
 
           specify { full_match_fails }
+          specify { unordered_match_fails }
           specify { partial_match_raises_error }
         end
 
@@ -57,6 +73,7 @@ RSpec.describe 'match_result_set' do
           let(:expected_rows) { [john.values] }
 
           specify { full_match }
+          specify { unordered_match }
           specify { partial_match_raises_error }
         end
 
@@ -64,6 +81,7 @@ RSpec.describe 'match_result_set' do
           let(:expected_rows) { [[a_string_matching('John'), a_string_matching(/lennon/i), 40]] }
 
           specify { full_match }
+          specify { unordered_match }
           specify { partial_match_raises_error }
         end
       end
@@ -73,28 +91,106 @@ RSpec.describe 'match_result_set' do
           let(:expected_rows) { [john.dup.tap { |j| j[:ono] = 'yoko' }] }
 
           specify { full_match_fails }
+          specify { unordered_match_fails }
           specify { partial_match_fails }
+          specify { partial_unordered_match_fails }
         end
 
         context 'when the actual and expected have are different' do
           let(:expected_rows) { [john.dup.tap { |j| j[:first_name] = 'yoko' }] }
 
           specify { full_match_fails }
+          specify { unordered_match_fails }
           specify { partial_match_fails }
+          specify { partial_unordered_match_fails }
         end
 
         context 'when the actual and expected rows are equal' do
           let(:expected_rows) { [john] }
 
           specify { full_match }
+          specify { unordered_match }
           specify { partial_match }
+          specify { partial_unordered_match }
         end
 
         context 'when matching a subset of columns' do
           let(:expected_rows) { [{first_name: john[:first_name]}] }
 
           specify { full_match_fails }
+          specify { unordered_match_fails }
           specify { partial_match }
+          specify { partial_unordered_match }
+        end
+      end
+    end
+  end
+
+  context 'when the expected set has multiple rows' do
+    let(:ringo) { {first_name: 'Richard', last_name: 'Starkey', age: -75} }
+
+    context 'and the actual set has the same number of rows' do
+      let(:actual_rows) { [ringo, paul, john] }
+
+      context 'when the row is given as an array' do
+        context 'when rows are returned in order' do
+          let(:expected_rows) { [ringo.values, paul.values, john.values] }
+
+          specify { full_match }
+          specify { unordered_match }
+          specify { partial_match_raises_error }
+        end
+
+        context 'when rows are returned in different order' do
+          let(:expected_rows) { [paul.values, john.values, ringo.values] }
+
+          specify { full_match_fails }
+          specify { unordered_match }
+          specify { partial_match_raises_error }
+        end
+      end
+
+      context 'when the row is given as a hash' do
+        context 'when matching all columns' do
+          context 'when rows are returned in order' do
+            let(:expected_rows) { [ringo, paul, john] }
+
+            specify { full_match }
+            specify { unordered_match }
+            specify { partial_match }
+            specify { partial_unordered_match }
+          end
+
+          context 'when rows are returned in different order' do
+            let(:expected_rows) { [paul, john, ringo] }
+
+            specify { full_match_fails }
+            specify { unordered_match }
+            specify { partial_match_fails }
+            specify { partial_unordered_match }
+          end
+        end
+
+        context 'when matching a subset of columns' do
+          let(:expected_rows) { members.map { |member| {age: member[:age]} } }
+
+          context 'when rows are returned in order' do
+            let(:members) { [ringo, paul, john] }
+
+            specify { full_match_fails }
+            specify { unordered_match_fails }
+            specify { partial_match }
+            specify { partial_unordered_match }
+          end
+
+          context 'when rows are returned in different order' do
+            let(:members) { [john, paul, ringo] }
+
+            specify { full_match_fails }
+            specify { unordered_match_fails }
+            specify { partial_match_fails }
+            specify { partial_unordered_match }
+          end
         end
       end
     end
