dd5d4378b712cc84c8761b84fd9b692a1a2750f5
Lukasz Madlewski <lukasz.madlewski@u2i.com>
2017-10-06 13:52:42 +0200

    Reimplement load file to hive table due to string literal restriction

--- lib/rspec/hive/connection_delegator.rb
+++ lib/rspec/hive/connection_delegator.rb
@@ -85,10 +85,9 @@ module RSpec
       end
 
       def load_file_to_hive_table(table_name, path, partition_clause = nil)
-        request_txt =
-          "load data local inpath '#{path}' into table #{table_name}"
-        request_txt << " #{partition_clause}" unless partition_clause.nil?
-        execute(request_txt)
+        request_txt = "load data local inpath '#{path}' into table #{table_name}"
+        return execute(request_txt) if partition_clause.nil?
+        execute("#{request_txt} #{partition_clause}")
       end
 
       def docker_path(file)
